30918636656a28602c164764b606e580bd20a8d7
Lucas Alves Da Silva <lukas-r8@10-20-52-86.0200.fed.it.iosda.org>
2019-01-22 17:45:18 +0100

    test

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use((req,res,next) => {
 })
 
 app.use((req,res,next) => {
-  console.log('===',new Date().toString(),req.method, req.url);
+  console.log('====',new Date().toString(),req.method, req.url);
   next();
 });
 
