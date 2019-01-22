c95fa8db632fe62c2e329b50d72f5b7209571667
Lucas Alves Da Silva <lukas-r8@Lucass-MacBook-Pro.local>
2019-01-22 23:49:56 +0100

    adding log file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
@@ -7,13 +8,14 @@ hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'));
 
+//
+// app.use((req,res,next) => {
+//   res.render('Maintenance.hbs')
+// });
 
 app.use((req,res,next) => {
-  res.render('Maintenance.hbs')
-})
-
-app.use((req,res,next) => {
-  console.log('====',new Date().toString(),req.method, req.url);
+  var log =`${new Date().toString()}, ${req.method}, ${req.url}\n`
+  fs.appendFile('log.txt', log, (err) => { if (err) {console.log(err)}});
   next();
 });
 
